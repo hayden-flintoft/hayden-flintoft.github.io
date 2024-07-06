3f7b274a3ed3ddf86021dbdedb7587d74fee0425
Hayden Flintoft <hflintoft@gmail.com>
2024-07-06 12:57:23 +1200

    Add new blog post on neuroplasticity Add new blog post on Te Whare Tapa Whā Update navigation to include new blog posts Implement navigation script

---
+++ scripts/navigation.js
@@ -0,0 +1,34 @@
+function includeRootNav() {
+  fetch('https://hayden-flintoft.github.io/templates/nav-root.html')
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById('nav').innerHTML = data
+      highlightActiveLink()
+    })
+    .catch((error) => console.error('Error loading the navigation:', error))
+}
+
+function includeChildNav() {
+  fetch('../templates/nav.html') // Adjust the path to fetch from child pages
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById('nav').innerHTML = data
+      highlightActiveLink()
+    })
+    .catch((error) => console.error('Error loading the navigation:', error))
+}
+
+function highlightActiveLink() {
+  const links = document.querySelectorAll('#nav a')
+  const currentPath = window.location.pathname.replace(/\/$/, '') // Remove trailing slash for uniformity
+
+  links.forEach((link) => {
+    const linkPath = new URL(link.href, window.location.origin).pathname.replace(/\/$/, '') // Normalize the path
+    // Check if the current pathname ends with the href attribute of the link
+    if (currentPath === linkPath || (currentPath === '' && linkPath === '/index.html')) {
+      link.classList.add('active')
+    } else {
+      link.classList.remove('active')
+    }
+  })
+}
--- scripts/scripts.js
+++
@@ -1,7 +0,0 @@
-function includeNav() {
-  fetch('https://hayden-flintoft.github.io/templates/nav.html')
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById('nav').innerHTML = data
-    })
-}
